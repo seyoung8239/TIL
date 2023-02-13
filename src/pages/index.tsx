5cbb4c056cce2881f32f9c96d55320ee30e05eb3
seyoung8239 <seyoung8239@gmail.com>
2023-02-13 17:02:44 +0900

    add dummy markdown data and graphql query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import GlobalStyle from '@components/common/GlobalStyle';
 import ProfileImage from '@components/main/ProfileImage';
+import { graphql } from 'gatsby';
 
 const Container = styled.div`
 	display: flex;
@@ -9,9 +10,35 @@ const Container = styled.div`
 	height: 100%;
 `;
 
-const IndexPage: FunctionComponent = function () {
+type PropsType = {
+	data: {
+		allMarkdownRemark: {
+			edges: [
+				{
+					node: {
+						id: string;
+						frontmatter: {
+							title: string;
+							summary: string;
+							date: string;
+							categories: string[];
+						};
+					};
+				},
+			];
+		};
+	};
+};
+
+const IndexPage: FunctionComponent<PropsType> = function ({
+	data: {
+		allMarkdownRemark: { edges },
+	},
+}) {
+	console.log(edges);
 	return (
 		<Container>
+			{JSON.stringify(edges)}
 			<GlobalStyle />
 			<ProfileImage />
 		</Container>
@@ -19,3 +46,26 @@ const IndexPage: FunctionComponent = function () {
 };
 
 export default IndexPage;
+
+export const getPostList = graphql`
+	query getPostList {
+		allMarkdownRemark(
+			sort: {
+				order: DESC
+				fields: [frontmatter___date, frontmatter___title]
+			}
+		) {
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						summary
+						date(formatString: "YYYY.MM.DD.")
+						categories
+					}
+				}
+			}
+		}
+	}
+`;
