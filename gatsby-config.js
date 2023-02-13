db0a13974c95c07deac648ea1ec0a202fbe7fe95
seyoung8239 <seyoung8239@gmail.com>
2023-02-13 15:30:26 +0900

    set up markdown config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`,
+		title: `My TIL Blog`,
+		description: `light blog I write down what I learn today`,
+		author: `@seyoung8239`,
 		siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
 	},
 	plugins: [
@@ -27,8 +27,8 @@ module.exports = {
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
-				name: `images`,
-				path: `${__dirname}/src/images`,
+				name: `contents`,
+				path: `${__dirname}/contents`,
 			},
 		},
 		`gatsby-transformer-sharp`,
@@ -45,6 +45,44 @@ module.exports = {
 				// theme_color: `#663399`,
 				display: `minimal-ui`,
 				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: 'gatsby-remark-smartypants',
+						options: {
+							dashes: 'oldschool',
+						},
+					},
+					{
+						resolve: 'gatsby-remark-prismjs',
+						options: {
+							classPrefix: 'language-',
+						},
+					},
+					{
+						resolve: 'gatsby-remark-images',
+						options: {
+							maxWidth: 768,
+							quality: 100,
+							withWebp: true,
+						},
+					},
+					{
+						resolve: 'gatsby-remark-copy-linked-files',
+						options: {},
+					},
+					{
+						resolve: 'gatsby-remark-external-links',
+						options: {
+							target: '_blank',
+							rel: 'nofollow',
+						},
+					},
+				],
 			},
 		},
 	],
