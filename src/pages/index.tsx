d224f95d50cf456cd0c7cbdb33077944e35d83b7
seyoung8239 <seyoung8239@gmail.com>
2023-02-16 15:22:11 +0900

    remove redundent files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,74 +1,14 @@
 import React, { FunctionComponent } from 'react';
-import styled from '@emotion/styled';
+import Main from '@containers/Main';
 import GlobalStyle from '@components/@common/GlobalStyle';
-import { graphql } from 'gatsby';
 
-const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-	height: 100%;
-`;
-
-type PropsType = {
-	data: {
-		allMarkdownRemark: {
-			edges: [
-				{
-					node: {
-						id: string;
-						frontmatter: {
-							title: string;
-							summary: string;
-							date: string;
-							categories: string[];
-						};
-						fields: {
-							slug: string;
-						};
-					};
-				},
-			];
-		};
-	};
-};
-
-const IndexPage: FunctionComponent<PropsType> = function ({
-	data: {
-		allMarkdownRemark: { edges },
-	},
-}) {
+const IndexPage: FunctionComponent = function () {
 	return (
-		<Container>
-			{JSON.stringify(edges)}
+		<>
 			<GlobalStyle />
-		</Container>
+			<Main />
+		</>
 	);
 };
 
 export default IndexPage;
-
-export const getPostList = graphql`
-	query getPostList {
-		allMarkdownRemark(
-			sort: {
-				order: DESC
-				fields: [frontmatter___date, frontmatter___title]
-			}
-		) {
-			edges {
-				node {
-					id
-					frontmatter {
-						title
-						summary
-						date(formatString: "YYYY.MM.DD.")
-						categories
-					}
-					fields {
-						slug
-					}
-				}
-			}
-		}
-	}
-`;
