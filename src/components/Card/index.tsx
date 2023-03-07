c345ec5cac15c3542ea90e340c55ac25466d6837
seyoung8239 <seyoung8239@gmail.com>
2023-03-07 17:56:01 +0900

    게시글 상세 navigation 추가

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,21 @@
 import React, { FC, FunctionComponent } from 'react';
+import { navigate } from 'gatsby';
 import * as S from './styles';
 import { PostType } from '@customTypes/gql';
 
 interface propsType {
-	data: Pick<PostType, 'frontmatter'>;
+	data: Pick<PostType, 'frontmatter' | 'fields'>;
 }
 
-const Card: FunctionComponent<propsType> = ({ data: { frontmatter } }) => {
+const Card: FunctionComponent<propsType> = ({
+	data: { frontmatter, fields },
+}) => {
 	const { title, summary, createdAt, categories } = frontmatter;
+	const handleClickCard = () => {
+		navigate('/post' + fields.slug);
+	};
 	return (
-		<S.Card>
+		<S.Card onClick={handleClickCard}>
 			<S.Title>{title}</S.Title>
 			<S.CreatedAt>{createdAt}</S.CreatedAt>
 			<S.Summary>{summary}</S.Summary>
