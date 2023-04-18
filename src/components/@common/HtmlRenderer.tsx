f0ec366f5c42a901fcdef9a2196649a4eec6872a
seyoung8239 <seyoung8239@gmail.com>
2023-04-19 04:38:12 +0900

    marker indentation 수정

--- src/components/@common/HtmlRenderer.tsx
+++ src/components/@common/HtmlRenderer.tsx
@@ -1,38 +1,9 @@
-import styled from '@emotion/styled';
 import React, { FunctionComponent } from 'react';
+import { ContentWrapper } from '@styles/HtmlRendererStyle';
 
 interface PropsType {
 	htmlString: string;
 }
-
-const ContentWrapper = styled.div`
-	padding: 40px;
-	width: 100%;
-	max-width: 800px;
-	min-height: 95vh;
-	a {
-		color: green;
-	}
-	p {
-		margin-bottom: 15px;
-		line-height: 25px;
-	}
-	h1 {
-		margin: 30px 0px;
-	}
-	h2 {
-		margin: 20px 0px;
-		width: 100%;
-		background-color: #d5e3d3;
-		line-height: 180%;
-		padding-left: 5px;
-	}
-	h3,
-	h4 {
-		color: #435440;
-		margin: 10px 0px;
-	}
-`;
 
 const HtmlRenderer: FunctionComponent<PropsType> = ({ htmlString }) => {
 	return <ContentWrapper dangerouslySetInnerHTML={{ __html: htmlString }} />;
---
+++ src/styles/HtmlRendererStyle.ts
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+export const ContentWrapper = styled.div`
+	padding: 40px;
+	width: 100%;
+	max-width: 800px;
+	min-height: 95vh;
+	a {
+		color: green;
+	}
+	p {
+		margin-bottom: 15px;
+		line-height: 25px;
+	}
+	h1 {
+		margin: 30px 0px;
+	}
+	h2 {
+		margin: 20px 0px;
+		width: 100%;
+		background-color: #d5e3d3;
+		line-height: 180%;
+		padding-left: 5px;
+	}
+	h3,
+	h4 {
+		color: #435440;
+		margin: 10px 0px;
+	}
+	ol,
+	ul {
+		margin-left: 20px;
+		padding-left: 0;
+		margin-bottom: 15px;
+	}
+    li {
+        margin-bottom: 5px;
+    }
+`;
